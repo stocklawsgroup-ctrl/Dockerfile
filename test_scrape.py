aa183c0e520caf84253848f736ecf0cc788fc45d
stocklawsgroup-ctrl <stocklawsgroup@gmail.com>
2026-03-16 15:56:13 +0900

    Fix formatting in test_scrape.py

--- test_scrape.py
+++ test_scrape.py
@@ -11,7 +11,7 @@ async def run(self, p):
           browser = await p.chromium.launch(
               headless=True,
               args=[
-                  "--no-sandbox",
+                  "--no-sandbox", 
                   "--disable-dev-shm-usage",
                   "--disable-blink-features=AutomationControlled",
                   "--disable-gpu",
